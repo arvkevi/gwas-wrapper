80256a8348d92ccd6ca1151b504d08e3e3a42628
arvkevi <arvkevi@gmail.com>
2016-04-17 10:46:58 -0400

    For PyPI

--- setup.py
+++ setup.py
@@ -10,12 +10,14 @@ setup(name='gwas_wrapper',
 	  description='Python wrapper for interacting with the NHGRI-EBI GWAS Catalog',
 	  url='http://github.com/arvkevi/gwas_wrapper',
 	  license='MIT',
-	  keywords='gwas genomics snp bioinformatics',
+	  keywords=['gwas', 'genomics', 'snp', 'bioinformatics'],
 	  classifiers=[
 		'Programming Language :: Python :: 2.7',
 		'Topic :: Scientific/Engineering :: Bio-Informatics'
 		],
 	  author='Kevin Arvai',
 	  author_email='arvkevi@gmail.com',
+	  download_url = 'https://github.com/arvkevi/gwas_wrapper/tarball/0.1',
+	  url = 'https://github.com/arvkevi/gwas_wrapper',
 	  packages=['gwas_wrapper'],
 	  zip_safe=False)
